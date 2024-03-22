c29748bba84a83a913b45891569b5289c657af8e
Md. Mamun Hossain <muhammed.mamun.131@gmail.com>
2024-03-22 10:30:26 +0600

    log in page done

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,11 @@ import Login from "./Components/Login/Login";
 
 function App() {
   return (
-    <div className="container-full min-h-screen bg-white">
-        <Login></Login>
+    <div className="container-full h-screen  bg-[#d9d9d9]">
+        
+          <Login></Login>
+
+        
     </div>
   );
 }
--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,15 +1,99 @@
-
+import capImg from '../../assets/Cap.png';
+import heroImg from '../../assets/left-bg.png';
 const Login = () => {
-    return (
-        <div className="flex pt-32">
-            <div className="w-7/12 pt-20">
-                <h1 className="text-4xl text-center">Cubicles Management System</h1>
+  return (
+    <div className="flex h-screen ">
+      <div className="w-7/12 bg-cover bg-no-repeat" style={{
+        backgroundImage: `url(${heroImg})`
+      }}>
+        <div class="w-full h-full flex 
+             bg-blue-600/30 backdrop-brightness-75">
+               <div className='pl-4 pt-10'>
+
+            <div className='flex mb-5'>
+                <img src={capImg} alt="cap" /> 
+                <h2 className='text-4xl text-white'>UTCMS</h2>   
+            </div>            
+            <div>
+                <h2 className='text-3xl text-white underline'>As Teacher:</h2>
+                <ul className='list-disc text-white ml-10'>
+                    <li>Connect With You Students</li>
+                    <li>Get Schedule Based Notifications</li>
+                    <li>Manage Meetings</li>
+                    <li>Manage Announcements</li>
+                    <li>Manage Academic Progress </li>
+                </ul>
+
             </div>
-            <div className="w-5/12">
-            <h2>log in </h2>
+            <div className='pt-2'>
+                <h2 className='text-3xl text-white underline'>As Teacher:</h2>
+                <ul className='list-disc text-white ml-10'>
+                    <li>Connect With You Students</li>
+                    <li>Get Schedule Based Notifications</li>
+                    <li>Manage Meetings</li>
+                    <li>Manage Announcements</li>
+                    <li>Manage Academic Progress </li>
+                </ul>
+
             </div>
+
+            
+        <button class="bg-[#d8d8d8] max-w-xs mt-20 ml-10 hover:bg-blue-900  text-[#202934] font-bold py-2 px-4 rounded-lg w-full pb-4 ">
+              Learn more
+            </button>
+        </div> 
         </div>
-    );
+        
+        
+      </div>
+      <div className="w-5/12 pt-24">
+        <div className="w-8/12 px-4 py-10 rounded-lg shadow-sm">
+          <div className="pb-2">
+            <h3 className="text-2xl text-[#202934]">Sign in</h3>
+            <span className="text-xs pb-4 text-[#202934]">
+              sign in with your email and password
+            </span>
+          </div>
+          <hr className="h-[2px] bg-black mx-4 mb-4" />
+          <div>
+            <label className="input bg-white input-bordered flex items-center gap-2 mb-2">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 16 16"
+                fill="currentColor"
+                className="w-4 h-4 opacity-70"
+              >
+                <path d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" />
+                <path d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" />
+              </svg>
+              <input type="text" className="grow" placeholder="Email" />
+            </label>
+
+            <label className="input input-bordered bg-white  flex items-center gap-2 mb-2">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 16 16"
+                fill="currentColor"
+                className="w-4 h-4 opacity-70"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
+                  clipRule="evenodd"
+                />
+              </svg>
+              <input type="password" className="grow" placeholder="Password" />
+            </label>
+           <div className="w-full flex justify-center">
+           <button class="bg-[#3365e1] max-w-xs mx-auto hover:bg-blue-900  text-[#D9D9D9] font-bold py-2 px-4 rounded-lg w-full">
+              Log in
+            </button>
+           </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
