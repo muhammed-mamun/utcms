a02253acf3c5f063691aa4ceaee6c791465e225f
Md. Mamun Hossain <muhammed.mamun.131@gmail.com>
2024-03-22 07:17:03 +0600

    Poject temlate is ready

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Login from "./Components/Login/Login";
+
+function App() {
+  return (
+    <div className="container-full min-h-screen bg-white">
+        <Login></Login>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Login/Login.jsx
@@ -0,0 +1,15 @@
+
+const Login = () => {
+    return (
+        <div className="flex pt-32">
+            <div className="w-7/12 pt-20">
+                <h1 className="text-4xl text-center">Cubicles Management System</h1>
+            </div>
+            <div className="w-5/12">
+            <h2>log in </h2>
+            </div>
+        </div>
+    );
+};
+
+export default Login;
